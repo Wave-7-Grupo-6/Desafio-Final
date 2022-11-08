bc8385db3d9b6e36be75be2c1690cab96a4979c5
thays-gama <thays.gama@mercadolivre.com>
2022-11-08 16:01:05 -0300

    adiciona realacionamentos no purchase

--- src/main/java/br/com/dh/meli/desafiofinal/model/PurchaseItem.java
+++ src/main/java/br/com/dh/meli/desafiofinal/model/PurchaseItem.java
@@ -1,6 +1,8 @@
 package br.com.dh.meli.desafiofinal.model;
 
 import br.com.dh.meli.desafiofinal.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,11 +12,12 @@ import java.math.BigDecimal;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class PurchaseItem {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
@@ -22,9 +25,12 @@ public class PurchaseItem {
 
     private BigDecimal price;
 
-    p
+    @ManyToOne
+    @JoinColumn(name = "annoucement_id", nullable = false)
+    private Annoucement annoucement;
 
     @ManyToOne
-    @JoinColumn(name = "purchase_order_id")
+    @JoinColumn(name = "purchase_order_id", nullable = false)
+    @JsonIgnoreProperties("purchaseItems")
     private PurchaseOrder purchaseOrder;
 }
--- src/main/java/br/com/dh/meli/desafiofinal/model/PurchaseOrder.java
+++ src/main/java/br/com/dh/meli/desafiofinal/model/PurchaseOrder.java
@@ -1,7 +1,10 @@
 package br.com.dh.meli.desafiofinal.model;
 
 import br.com.dh.meli.desafiofinal.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -12,9 +15,11 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PurchaseOrder {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
@@ -26,6 +31,11 @@ public class PurchaseOrder {
     @Transient
     private BigDecimal totalPrice;
 
-    @OneToMany(mappedBy = "purchaseOrder")
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @OneToMany(mappedBy = "purchaseOrder", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties("purchaseOrder")
     private Set<PurchaseItem> purchaseItems;
 }
