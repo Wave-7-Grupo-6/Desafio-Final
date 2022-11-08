a4ed669df4468ba1c9c0a642660ca17eea5c3071
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-08 14:52:52 -0300

    feat: create model of Annoucement

---
+++ src/main/java/br/com/dh/meli/desafiofinal/model/Annoucement.java
@@ -0,0 +1,36 @@
+package br.com.dh.meli.desafiofinal.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Annoucement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @JsonIgnoreProperties("annoucements")
+    @JsonBackReference
+    private Category category;
+}
