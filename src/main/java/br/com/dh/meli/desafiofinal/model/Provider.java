513503f0875b4c775bc0eff811f1d9426a9939b0
maurocorreia <mauroac1@outlook.com>
2022-11-08 17:51:12 -0300

    feat: Warehouse and Provider Entity.

---
+++ src/main/java/br/com/dh/meli/desafiofinal/model/Provider.java
@@ -0,0 +1,26 @@
+package br.com.dh.meli.desafiofinal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Provider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "provider", cascade = CascadeType.REFRESH)
+    private List<Warehouse> warehouse;
+}
