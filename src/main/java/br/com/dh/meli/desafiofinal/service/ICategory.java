878cab690ceaeb88f8cdb02ad041e4448ef675ad
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-09 18:35:18 -0300

    feat: Category api and dto

--- src/main/java/br/com/dh/meli/desafiofinal/service/ICategory.java
+++ src/main/java/br/com/dh/meli/desafiofinal/service/ICategory.java
@@ -1,8 +1,13 @@
 package br.com.dh.meli.desafiofinal.service;
 
+import br.com.dh.meli.desafiofinal.dto.CategoryDTO;
 import br.com.dh.meli.desafiofinal.model.Category;
+
+import java.util.List;
 
 public interface ICategory {
     Category findById(Long id);
     Category findByName(String name);
+    void save(CategoryDTO categoryDTO);
+    List<Category> findAll();
 }
