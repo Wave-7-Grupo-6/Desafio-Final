0573164b30d47895144c9ab60d0055c59430ac43
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-09 09:44:09 -0300

    fix: attributes with errors

--- src/main/java/br/com/dh/meli/desafiofinal/model/Annoucement.java
+++ src/main/java/br/com/dh/meli/desafiofinal/model/Annoucement.java
@@ -2,14 +2,13 @@ package br.com.dh.meli.desafiofinal.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Getter
