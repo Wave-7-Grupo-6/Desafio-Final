5b8ec30647fa5f76551a53cae45b13071b895eab
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-09 12:09:09 -0300

    feat: search product by category

--- src/main/java/br/com/dh/meli/desafiofinal/service/IAnnouncement.java
+++ src/main/java/br/com/dh/meli/desafiofinal/service/IAnnouncement.java
@@ -2,7 +2,10 @@ package br.com.dh.meli.desafiofinal.service;
 
 import br.com.dh.meli.desafiofinal.model.Annoucement;
 
+import java.util.List;
+
 public interface IAnnouncement {
     Annoucement findById(Long id);
     Annoucement save(Annoucement annoucement);
+    List<Annoucement> findByCategory(String category);
 }
