78a76027484f84649dd8218ed547a5e5244bfce1
Michael Caxias <michael.caxias@mercadolivre.com>
2022-11-10 17:31:04 -0300

    test: 🧪 getAll de WarehouseControllerTest em caso de sucesso

---
+++ src/test/java/br/com/dh/meli/desafiofinal/controller/WarehouseControllerTest.java
@@ -0,0 +1,66 @@
+package br.com.dh.meli.desafiofinal.controller;
+
+import br.com.dh.meli.desafiofinal.model.Warehouse;
+import br.com.dh.meli.desafiofinal.service.ICategory;
+import br.com.dh.meli.desafiofinal.service.IWarehouse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import br.com.dh.meli.desafiofinal.dto.CategoryDTO;
+import br.com.dh.meli.desafiofinal.model.Category;
+import br.com.dh.meli.desafiofinal.service.ICategory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static br.com.dh.meli.desafiofinal.utils.TestUtils.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.hamcrest.CoreMatchers;
+
+import java.util.List;
+
+@WebMvcTest(CategoryController.class)
+public class WarehouseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private IWarehouse warehouseService;
+
+
+    @Test
+    void getAll_returnsWarehouses_whenSuccess() throws Exception {
+        List<Warehouse> warehouses = List.of(getWarehouse());
+
+        when(warehouseService.findAll()).thenReturn(warehouses);
+
+        ResultActions resultActions = mockMvc.perform(
+                get("/api/v1/warehouse")
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", CoreMatchers.is(getWarehouse().getId().intValue())))
+                .andExpect(jsonPath("$[0].name", CoreMatchers.is(getWarehouse().getName())));
+    }
+}
