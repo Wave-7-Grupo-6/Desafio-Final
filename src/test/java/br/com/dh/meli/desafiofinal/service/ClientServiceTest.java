b7cbc81c8dc5de715fb15edd74be6f2f38d66843
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-10 12:43:22 -0300

    feat: test findById with that's ok

---
+++ src/test/java/br/com/dh/meli/desafiofinal/service/ClientServiceTest.java
@@ -0,0 +1,49 @@
+package br.com.dh.meli.desafiofinal.service;
+
+import br.com.dh.meli.desafiofinal.model.Client;
+import br.com.dh.meli.desafiofinal.repository.ClientRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static br.com.dh.meli.desafiofinal.utils.TestUtils.getClient;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ClientServiceTest {
+    @Mock
+    private ClientRepository repository;
+
+    private IClient service;
+
+    @BeforeEach
+    void setup(){
+        service = new ClientService(repository);
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+        when(repository.findById(anyLong())).thenReturn(Optional.of(getClient()));
+
+        Client client = service.findById(1L);
+
+        assertThat(client).isNotNull();
+        assertThat(client.getId()).isEqualTo(getClient().getId());
+        assertThat(client.getName()).isEqualTo(getClient().getName());
+        assertThat(client.getCarts()).isNull();
+    }
+}
