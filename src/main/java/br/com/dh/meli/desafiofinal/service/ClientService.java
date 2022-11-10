ac6b7c36423c5c8c577482c33819b333a0b6484c
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-10 12:41:21 -0300

    fix client findById

--- src/main/java/br/com/dh/meli/desafiofinal/service/ClientService.java
+++ src/main/java/br/com/dh/meli/desafiofinal/service/ClientService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,11 +25,7 @@ public class ClientService implements IClient{
     }
 
     @Override
-    public Optional<Client> findById(Long id) {
-        Optional<Client> opClient = repo.findById(id);
-
-        if(opClient.isEmpty()) throw new NoSuchElementException("Client not found!");
-
-        return opClient;
+    public Client findById(Long id) {
+        return repo.findById(id).orElseThrow(() -> new NoSuchElementException("Client not found"));
     }
 }
