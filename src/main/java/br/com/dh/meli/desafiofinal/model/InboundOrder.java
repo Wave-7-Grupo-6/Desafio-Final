be7345eba60e254124d06bbc3d0b679a316ab59d
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-11 11:27:13 -0300

    fix: refactoring simplicate createAttributes

--- src/main/java/br/com/dh/meli/desafiofinal/model/InboundOrder.java
+++ src/main/java/br/com/dh/meli/desafiofinal/model/InboundOrder.java
@@ -45,15 +45,11 @@ public class InboundOrder {
     @JsonBackReference
     private Section section;
 
-    public InboundOrder(InboundOrderDTO inboundOrderDTO, Section section, List<Announcement> annoucements) {
+    public InboundOrder(InboundOrderDTO inboundOrderDTO, Section section) {
         this.id = inboundOrderDTO.getId();
         this.orderDate = inboundOrderDTO.getOrderDate();
         this.orderNumber = inboundOrderDTO.getOrderNumber();
         this.section = section;
         this.batchs = new ArrayList<>();
-        for(int i = 0; i < inboundOrderDTO.getBatchStockDTOList().size(); i++){
-            BatchStockDTO batchStockDTO = inboundOrderDTO.getBatchStockDTOList().get(i);
-            this.batchs.add(batchStockDTO.createBatch(batchStockDTO, this, annoucements.get(i)));
-        }
     }
 }
