569ebf9e8a3787676c43451d40e78dc5371258f1
Michael Caxias <michael.caxias@mercadolivre.com>
2022-11-13 19:31:13 -0300

    test: 🧪 BatchServiceTest 100% dos métodos e linhas testados

---
+++ src/test/java/br/com/dh/meli/desafiofinal/service/BatchServiceTest.java
@@ -0,0 +1,47 @@
+package br.com.dh.meli.desafiofinal.service;
+
+import br.com.dh.meli.desafiofinal.model.Batch;
+import br.com.dh.meli.desafiofinal.repository.BatchRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static br.com.dh.meli.desafiofinal.utils.TestUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BatchServiceTest {
+
+    @Mock
+    private BatchRepository repository;
+
+    private IBatch service;
+
+    @BeforeEach
+    void setUp() {
+        service = new BatchService(repository);
+    }
+
+    @Test
+    void save_returnBatch_whenValid() {
+        Batch batch = getBatch();
+        when(repository.save(batch)).thenReturn(batch);
+
+        Batch savedBatch = service.save(batch);
+
+        assertThat(savedBatch).isNotNull();
+        assertThat(savedBatch.getBatchNumber()).isEqualTo(batch.getBatchNumber());
+        assertThat(savedBatch.getDueDate()).isEqualTo(batch.getDueDate());
+        assertThat(savedBatch.getManufacturingDate()).isEqualTo(batch.getManufacturingDate());
+    }
+}
