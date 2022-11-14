7710b352176e9b24c39605ffbc6915f236efa5a9
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-14 12:23:42 -0300

    fix - resolve conflicts

--- src/main/java/br/com/dh/meli/desafiofinal/service/IBatch.java
+++ src/main/java/br/com/dh/meli/desafiofinal/service/IBatch.java
@@ -7,5 +7,7 @@ import java.util.List;
 
 public interface IBatch {
     Batch save(Batch batch);
-
+    List<BatchDTO> findByDueDateIsBefore(int days, Long sectionId, Long sellerId);
+    Batch updateStock(Long batchId, Long productId, Integer quantity);
+    Batch findById(Long id);
 }
