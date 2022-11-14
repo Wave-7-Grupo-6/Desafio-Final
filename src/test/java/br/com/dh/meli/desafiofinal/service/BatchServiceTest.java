43c3f669684268599662569f2c213aeeac8637f5
thays-gama <thays.gama@mercadolivre.com>
2022-11-14 15:17:03 -0300

    adiciona testes ao service de batch

--- src/test/java/br/com/dh/meli/desafiofinal/service/BatchServiceTest.java
+++ src/test/java/br/com/dh/meli/desafiofinal/service/BatchServiceTest.java
@@ -1,5 +1,7 @@
 package br.com.dh.meli.desafiofinal.service;
 
+import br.com.dh.meli.desafiofinal.exceptions.NotFoundException;
+import br.com.dh.meli.desafiofinal.exceptions.OutOfStockException;
 import br.com.dh.meli.desafiofinal.model.Batch;
 import br.com.dh.meli.desafiofinal.repository.BatchRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,15 +10,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
-import static br.com.dh.meli.desafiofinal.utils.TestUtils.*;
+import static br.com.dh.meli.desafiofinal.utils.TestUtils.getBatch;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,5 +43,55 @@ public class BatchServiceTest {
         assertThat(savedBatch.getBatchNumber()).isEqualTo(batch.getBatchNumber());
         assertThat(savedBatch.getDueDate()).isEqualTo(batch.getDueDate());
         assertThat(savedBatch.getManufacturingDate()).isEqualTo(batch.getManufacturingDate());
+    }
+
+    @Test
+    void updateStock_returnBatch_whenValid() {
+        Batch batch = getBatch();
+        when(repository.findById(anyLong())).thenReturn(Optional.of(batch));
+        when(repository.save(any(Batch.class))).thenReturn(batch);
+
+        Batch updatedBatch = service.updateStock(batch.getBatchNumber(), batch.getAnnouncement().getId(), 5);
+
+        assertThat(updatedBatch).isNotNull();
+        assertThat(updatedBatch.getBatchNumber()).isEqualTo(batch.getBatchNumber());
+        assertThat(updatedBatch.getProductQuantity()).isEqualTo(batch.getProductQuantity());
+    }
+
+    @Test
+    void updateStock_throwsNotFoundException_whenBatchNotValid() {
+        when(repository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, ()-> service.updateStock(1L, 1L, 5));
+    }
+
+    @Test
+    void updateStock_throwsOutOfStockException_whenBatchHasNotEnoughtStock() {
+        Batch batch = getBatch();
+        when(repository.findById(anyLong())).thenReturn(Optional.of(batch));
+
+        assertThrows(OutOfStockException.class, ()-> service.updateStock(batch.getBatchNumber(), batch.getAnnouncement().getId(), 11));
+    }
+
+    @Test
+    void findById_throwsNotFoundException_whenBatchDoesntExist() {
+        when(repository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, ()-> service.findById(1L));
+    }
+
+
+    @Test
+    void findById_returnBatch_whenBatchExists() {
+        Batch batch = getBatch();
+        when(repository.findById(anyLong())).thenReturn(Optional.of(batch));
+
+        Batch foundBatch = service.findById(batch.getBatchNumber());
+
+        assertThat(foundBatch).isNotNull();
+        assertThat(foundBatch.getBatchNumber()).isEqualTo(batch.getBatchNumber());
+        assertThat(foundBatch.getProductQuantity()).isEqualTo(batch.getProductQuantity());
+        assertThat(foundBatch.getDueDate()).isEqualTo(batch.getDueDate());
+        assertThat(foundBatch.getManufacturingDate()).isEqualTo(batch.getManufacturingDate());
     }
 }
