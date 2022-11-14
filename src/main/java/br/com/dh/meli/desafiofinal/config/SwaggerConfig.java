5df757bdf81967074fd6b5d84dbea64eac30f441
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-11-14 17:16:26 -0300

    feat: application meta data

--- src/main/java/br/com/dh/meli/desafiofinal/config/SwaggerConfig.java
+++ src/main/java/br/com/dh/meli/desafiofinal/config/SwaggerConfig.java
@@ -3,11 +3,16 @@ package br.com.dh.meli.desafiofinal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +23,15 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+        return new ApiInfoBuilder().title("Desafio Final")
+                .description("Desafio Final para conclusão do curso de Java da DH")
+                .version("1.0.0")
+                .contact(new Contact("Grupo 06", "https://github.com/Wave-7-Grupo-6/Desafio-Final", null))
                 .build();
     }
 }
