b91f08616d6c6531d616aea4353109b9543c96be
Michael Caxias <michael.caxias@mercadolivre.com>
2022-11-18 18:43:11 -0300

    feat: cria corpo de retorno de api com CurrencyApiDTO

---
+++ src/main/java/br/com/dh/meli/desafiofinal/dto/CurrencyApiDTO.java
@@ -0,0 +1,25 @@
+package br.com.dh.meli.desafiofinal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyApiDTO implements Serializable {
+    private String code;
+    private String codein;
+    private String name;
+    private Float high;
+    private Float low;
+    private Float varBid;
+    private Float pctChange;
+    private Float bid;
+    private Float ask;
+    private String timestamp;
+    private LocalDateTime create_date;
+}
