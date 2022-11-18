e7910a14b28640b609eb9795c991ec5d094480e1
thays-gama <thays.gama@mercadolivre.com>
2022-11-18 11:56:48 -0300

    adiciona controller para role

--- src/main/java/br/com/dh/meli/desafiofinal/controller/RoleController.java
+++ src/main/java/br/com/dh/meli/desafiofinal/controller/RoleController.java
@@ -1,10 +1,13 @@
 package br.com.dh.meli.desafiofinal.controller;
 
+import br.com.dh.meli.desafiofinal.model.Role;
 import br.com.dh.meli.desafiofinal.service.IRole;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/role")
@@ -13,5 +16,30 @@ public class RoleController {
     @Autowired
     private IRole roleService;
 
+    @GetMapping
+    public ResponseEntity<List<Role>> findAll(){
+        return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Role> findById(@PathVariable Long id){
+        return new ResponseEntity<>(roleService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Role> save(@RequestBody Role role){
+        return new ResponseEntity<>(roleService.save(role), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Role> update(@RequestBody Role role){
+        return new ResponseEntity<>(roleService.update(role), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Role> deleteById(@PathVariable Long id){
+        roleService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
