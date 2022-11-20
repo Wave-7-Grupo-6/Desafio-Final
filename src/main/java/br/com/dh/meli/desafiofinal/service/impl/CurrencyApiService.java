53d0549cdd3fb57f388fa314935721765621c30a
Michael Caxias <michael.caxias@mercadolivre.com>
2022-11-20 19:28:45 -0300

    feat: tratamento de erro em getvalue de CurrencyApiService

--- src/main/java/br/com/dh/meli/desafiofinal/service/impl/CurrencyApiService.java
+++ src/main/java/br/com/dh/meli/desafiofinal/service/impl/CurrencyApiService.java
@@ -5,6 +5,7 @@ import br.com.dh.meli.desafiofinal.exceptions.NotFoundException;
 import br.com.dh.meli.desafiofinal.service.ICurrencyApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
@@ -14,12 +15,16 @@ import java.math.BigDecimal;
 public class CurrencyApiService implements ICurrencyApi {
     @Override
     public BigDecimal getValue(String currency) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = String.format("https://economia.awesomeapi.com.br/%s-BRL/?format=json", currency);
-        CurrencyApiDTO[] currencyApiDTO = restTemplate.getForObject(url, CurrencyApiDTO[].class);
-        if (currencyApiDTO == null) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = String.format("https://economia.awesomeapi.com.br/BRL-%s/?format=json", currency);
+            CurrencyApiDTO[] currencyApiDTO = restTemplate.getForObject(url, CurrencyApiDTO[].class);
+            if (currencyApiDTO == null) {
+                throw new NotFoundException("Currency not found.");
+            }
+            return currencyApiDTO[0].getBid();
+        } catch (HttpClientErrorException e) {
             throw new NotFoundException("Currency not found.");
         }
-        return currencyApiDTO[0].getBid();
     }
 }
