19f3820a42c28e1685c43312c9bdeecaa5f6f9e0
Paula Líbia Santos <paula.msantos@mercadolivre.com>
2022-11-23 08:52:03 -0300

    teste controller

--- src/test/java/br/com/dh/meli/desafiofinal/controller/FavoriteProductControllerTest.java
+++ src/test/java/br/com/dh/meli/desafiofinal/controller/FavoriteProductControllerTest.java
@@ -1,5 +1,8 @@
 package br.com.dh.meli.desafiofinal.controller;
 
+import br.com.dh.meli.desafiofinal.dto.ClientDTO;
+import br.com.dh.meli.desafiofinal.dto.ClientFavoritesDTO;
+import br.com.dh.meli.desafiofinal.model.Client;
 import br.com.dh.meli.desafiofinal.service.IClient;
 import br.com.dh.meli.desafiofinal.service.IFavoriteList;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,7 +10,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static br.com.dh.meli.desafiofinal.utils.TestUtils.getClient;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(FavoriteProductController.class)
 public class FavoriteProductControllerTest {
@@ -23,9 +36,6 @@ public class FavoriteProductControllerTest {
 
     @MockBean
     private IClient clientService;
-
-
-
 
 
 
